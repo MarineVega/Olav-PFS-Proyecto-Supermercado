dd54649f686966d69be9fe9cdc420348ceb9e8e8
MarineVega <marinev33@hotmail.com>
2024-07-22 18:32:33 -0300

    agregué página de contacto(básica) sin estilos y archivo js(básico) sin validaciones, que tiene la funcionalidad de guardar los datos de los input en un archivo txt

---
+++ js/contacto.js
@@ -0,0 +1,23 @@
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email");
+let telefono = document.getElementById("telefono");
+let consulta = document.getElementById("consulta");
+let btnEviar = document.getElementById("enviar");
+let infoContacto = [];
+
+btnEviar.addEventListener("click", (e) => {
+    e.preventDefault(); //previene la acción del form de actualizar la página
+    
+    infoContacto[0] = nombre.value;
+    infoContacto[1] = apellido.value;
+    infoContacto[2] = email.value;
+    infoContacto[3] = telefono.value;
+    infoContacto[4] = consulta.value;
+
+    console.log(`Su nombre es ${infoContacto[0]}`);
+
+    let blob = new Blob([infoContacto], { type: "text/plain;charset=utf-8"});
+
+    saveAs(blob, "contacto.txt");
+})
