b188579856b0f3f60ec4591e878cdf9cc9ccf248
MarineVega <marinev33@hotmail.com>
2024-07-25 16:32:38 -0300

    se agregó página productos con sus estilos y archivo js

--- js/productos.js
+++ js/productos.js
@@ -1,3 +1,4 @@
+/*
 const producto = {
     "nombre":"arroz",
     "cantidad":35,
@@ -51,5 +52,32 @@ productos[2].cantidad--
 console.log(productos[2].cantidad);
 
 document.querySelector("ul li").innerHTML = `El producto ${productos[0].nombre} tiene un stock de ${productos[0].cantidad}`;
+*/
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    const botonesAgregar = document.querySelectorAll('button');
+    const cantidadCarrito = document.getElementById('cantidad-carrito');
+    let totalimporte=0;
+    let totalItems = 0;
+    let importe=0;
+    let cantidad=0;
+    
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener('click', function() {
+            inputCantidad = this.closest('.items').querySelector('.entrada');
+            importe=this.closest('.items').querySelector('p').textContent;
+            cantidad = parseInt(inputCantidad.value);
+            
+            // aca quizas podriamos mejorarlo agregando en un array el producto con la cantidad 
+            // y si es el mismo que le modifican la cantidad buscarlo y colocarle la correcta.
+           
+            // Eliminar el signo '$' y convertir a número y saca el total
+            importe = parseFloat(importe.replace('$', ''))*cantidad;
+            totalItems += cantidad;
+            totalimporte +=importe;
+            cantidadCarrito.textContent = totalItems;
+            alert(totalimporte)
+        });
+    });
+});
